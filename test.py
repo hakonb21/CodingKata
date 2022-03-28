b90ba22bb6ecd1e2f9dfab3a04f6c9cd25d45933
dorajohanns <89972552+dorajohanns@users.noreply.github.com>
2022-03-28 11:27:00 +0000

    .

--- test.py
+++ test.py
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
 from kata import kata 
 
 def test_empty_string():
     assert kata("") == 0
-=======
-#test
->>>>>>> d492195cef3eaa126b0003f51c2e893e3369371c
+
+def test_int():
+    assert kata()
