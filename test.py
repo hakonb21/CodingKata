e93c9b3885fdd5fdb01b7fdb3f65e60d28749fe9
solsaem <sol21@ru.is>
2022-03-28 12:14:23 +0000

    öll test fyrir task 3 komin

--- test.py
+++ test.py
@@ -16,4 +16,9 @@ def test_support_two_numbers_1():
 def test_support_two_numbers_2():
     assert add("4,6") == 10
 
+def test_support_two_numbers_3():
+    for i in range(0,1000):
+        for j in range(0,1000):
+            assert add(f"{i},{j}") == i+j
+
 #4
