c9a8d3870a6329281cedf157990b5fdfb5d3a81e
Ymmiblubbs <ymir21@ru.is>
2022-03-28 11:29:15 +0000

    name changes

--- test.py
+++ test.py
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
-from kata import kata 
+from kata import add 
 
 def test_empty_string():
-    assert kata("") == 0
-=======
-#test
->>>>>>> d492195cef3eaa126b0003f51c2e893e3369371c
+    assert add("") == 0
+
+ 
