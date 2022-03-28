e122b9fcb90acb0c77871be0b16938ed38676a55
Ymmiblubbs <ymir21@ru.is>
2022-03-28 12:44:29 +0000

    kata supports multiple numbers

--- kata.py
+++ kata.py
@@ -2,10 +2,11 @@ def add(str):
     if str == "":
         return 0
     split = str.split(",")
-    if len(split) == 1:
-        return int(str)
-    if len(split) == 2:
-        return int(split[0]) + int(split[1])
+    sum = 0
+    for i in split:
+        sum += int(i)
+    return sum
+    
 
    
         
