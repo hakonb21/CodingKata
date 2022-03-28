df96a4a341c9bb3daa47d427649ea61d7b0046f4
Ymmiblubbs <ymir21@ru.is>
2022-03-28 11:32:46 +0000

    :

--- test.py
+++ test.py
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 from kata import add 
 
 def test_empty_string():
     assert add("") == 0
+    
 
  
-=======
-from kata import kata 
-
-def test_empty_string():
-    assert kata("") == 0
-
-def test_int():
-    assert kata()
->>>>>>> b90ba22bb6ecd1e2f9dfab3a04f6c9cd25d45933
